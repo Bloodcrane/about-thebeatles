a75de265468ee1404aa92289587f4aea966e7127
Bloodcrane <92672973+Bloodcrane@users.noreply.github.com>
2024-03-22 03:54:43 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Suspense, lazy } from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoaderLayout from './Layouts/Loader';
-import './App.css'; 
+import './App.css';
 
 const AboutPage = lazy(() => import('./Pages/AboutPage'));
 const FactsPage = lazy(() => import('./Pages/FactsPage'));
 const HomePage = lazy(() => import('./Pages/HomePage'));
 
 const App = () => (
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <Router basename={process.env.PUBLIC_URL}>
     <Suspense fallback={LoaderLayout}>
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/facts" element={<FactsPage />} />
         <Route path="/facts?page/:id" element={<FactsPage />} />
         <Route path="/facts/:page/:id" element={<FactsPage />} />
       </Routes>
     </Suspense>
-  </HashRouter>
+  </Router>
 );
 
 export default App;
