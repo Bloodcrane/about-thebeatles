fc760abdf0450aa75845c65bee8bf90e649df2e2
Bloodcrane <92672973+Bloodcrane@users.noreply.github.com>
2024-03-22 03:18:02 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React, { Suspense, lazy } from 'react';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import LoaderLayout from './Layouts/Loader.jsx';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import LoaderLayout from './Layouts/Loader';
 import './App.css';
 
-const AboutPage = lazy(() => import('./Pages/AboutPage.jsx'));
-const FactsPage = lazy(() => import('./Pages/FactsPage.jsx'));
-const HomePage = lazy(() => import('./Pages/HomePage.jsx'));
-const SingleFactsPage= lazy(() => import('./Pages/SingleFactsPage.jsx'));
+const AboutPage = lazy(() => import('./Pages/AboutPage'));
+const FactsPage = lazy(() => import('./Pages/FactsPage'));
+const HomePage = lazy(() => import('./Pages/HomePage'));
 
 const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Suspense fallback={LoaderLayout}>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/home" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/facts" element={<FactsPage />} />
-        <Route path="/facts/:page" element={<FactsPage />} />
-        <Route path="/fact/:title" element={<SingleFactsPage />} />
-        <Route path="/fact/:page/:id" element={<SingleFactsPage />} />
+        <Route path="/facts?page/:id" element={<FactsPage />} />
+        <Route path="/facts/:page/:id" element={<FactsPage />} />
       </Routes>
     </Suspense>
-  </Router>
+  </HashRouter>
 );
 
 export default App;
--- src/routers.js
+++ src/routers.js
@@ -1,38 +1,25 @@
-import HomePage from "./Pages/HomePage.jsx"
-import FactsPage from "./Pages/FactsPage.jsx"
-import AboutPage from "./Pages/AboutPage.jsx"
-import SingleFactsPage from "./Pages/SingleFactsPage.jsx"
+import HomePage from "./Pages/HomePage"
+import FactsPage from "./Pages/FactsPage"
+import AboutPage from "./Pages/AboutPage"
 
 const router = [
     {
-        element: <HomePage />,
+        element: <HomePage/>,
         path: '/',
         exact: true
     },
     {
-        element: <AboutPage />,
+        element: <AboutPage/>,
         path: '/about'
     },
     {
-        element: <FactsPage />,
+        element: <FactsPage/>,
         path: '/fact/:page/:id',
-        routes: [
-            {
-                element: <SingleFactsPage />,
-                path: '/fact/:page/:id'
-            }
-        ]
     },
     {
-      element: <FactsPage />,
-      path: '/fact',
-      routes: [
-          {
-              element: <SingleFactsPage />,
-              path: '/fact/:page/:id'
-          }
-      ]
-    },
+        element: <FactsPage/>,
+        path: '/fact'
+    }
 ]
 
 export default router
