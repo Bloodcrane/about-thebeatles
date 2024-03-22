394b7b9f6ef6ae93705f106e030d10fa01266298
Bloodcrane <92672973+Bloodcrane@users.noreply.github.com>
2024-03-22 04:34:29 +0400

    1

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
-import React from "react";
-import { Suspense, lazy } from 'react';
-import ReactDOM from "react-dom/client";
-import "./App.css";
+import React from 'react';
+import { createRoot, hydrateRoot } from 'react-dom/client';
+import App from './App';
 
-import LoaderLayout from './Layouts/Loader';
-
-import { HashRouter, Routes, Route } from "react-router-dom";
-const AboutPage = lazy(() => import('./Pages/AboutPage.jsx'));
-const FactsPage = lazy(() => import('./Pages/FactsPage.jsx'));
-const HomePage = lazy(() => import('./Pages/HomePage.jsx'));
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const StrictApp = () => (
   <React.StrictMode>
-    <HashRouter basename={`/${process.env.PUBLIC_URL}`}>
-      <Suspense fallback={LoaderLayout}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/facts" element={<FactsPage />} />
-          <Route path="/facts?page/:id" element={<FactsPage />} />
-          <Route path="/facts/:page/:id" element={<FactsPage />} />
-        </Routes>
-      </Suspense>
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
+
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, <StrictApp />);
+} else {
+  const root = createRoot(rootElement);
+  root.render(<StrictApp />);
+}
